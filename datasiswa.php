f897778ac1b4705833ddbf59f932e9d41243ee91
msashofwan123 <msashofwan123@gmail.com>
2023-01-19 08:57:36 +0700

    PAGINATION

--- datasiswa.php
+++ datasiswa.php
@@ -9,8 +9,8 @@ if (isset($_GET['cari'])) {
     $ambildata = mysqli_query($conn, $sqlsearch);
 } else {
 
-$sql = "SELECT datasiswa.*,daftar_kelas.id,kelas.nama_kelas FROM `datasiswa` LEFT JOIN daftar_kelas ON daftar_kelas.no_siswa=datasiswa.no LEFT JOIN kelas ON kelas.id=daftar_kelas.Id_kelas";
-$ambildata = mysqli_query($conn, $sql);
+    $sql = "SELECT datasiswa.*,daftar_kelas.id,kelas.nama_kelas FROM `datasiswa` LEFT JOIN daftar_kelas ON daftar_kelas.no_siswa=datasiswa.no LEFT JOIN kelas ON kelas.id=daftar_kelas.Id_kelas";
+    $ambildata = mysqli_query($conn, $sql);
 };
 
 
@@ -28,61 +28,70 @@ if ($_SESSION['nama_login'] == null) {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <link rel="stylesheet" href="index.css">
     <link href="favicon.ico" rel="icon" type="image/x-icon" />
+
+    <!-- DATA TABLE  -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.19/css/dataTables.bootstrap.min.css">
+
 </head>
 
 <body>
 
     <?php
     require_once('header.php');
-    
+
     if (isset($_GET['cari'])) {
         echo "<br>";
         echo "<h3><center>Data Ditemukan<center></h3>";
         echo "<br>";
     };
     ?>
-    <table class='table table-striped'>
-        <thead>
-            <tr>
-                <th>No</th>
-                <th>Nama</th>
-                <th>Alamat</th>
-                <th>Nomor HP</th>
-                <th>Gender</th>
-                <th>Email</th>
-                <th>Nama Kelas</th>
-                <th>Gambar</th>
-                <th>Aksi</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php $no = 1;
-            while ($data = mysqli_fetch_assoc($ambildata)) {
-                if ($data['file'] == "") {
-                    $gambar = "no-image.jpg";
-                } else {
-                    $gambar = $data['file'];
-                }
-            ?>
+    <div class="container">
+        <table class="table table-striped mytable" id="mytable">
+            <thead>
                 <tr>
-                    <th><?php echo $data['no'] ?></th>
-                    <td><?php echo $data['nama'] ?></td>
-                    <td><?= $data['alamat'] ?></td>
-                    <td><?= $data['nohp'] ?></td>
-                    <td><?= $data['gender'] ?></td>
-                    <td><?= $data['email'] ?></td>
-                    <td><?= $data['nama_kelas'] ?></td>
-                    <td><img class="img-thumbnail" width="100" src="assets/<?= $gambar ?>" /></td>
-                    <td>
-                        <button class="btn btn-info" data-bs-toggle="modal" data-bs-target="#exampleModal" data-bs-id="<?= $data['no'] ?>" data-bs-aksi="ubah"> Ubah
-                        </button>
-                        <button class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#exampleModal" data-bs-id="<?= $data['no'] ?>" data-bs-aksi="hapus"> Hapus
-                        </button>
-                    </td>
+                    <th>No</th>
+                    <th>ID</th>
+                    <th>Nama</th>
+                    <th>Alamat</th>
+                    <th>Nomor HP</th>
+                    <th>Gender</th>
+                    <th>Email</th>
+                    <th>Nama Kelas</th>
+                    <th>Gambar</th>
+                    <th>Aksi</th>
                 </tr>
-            <?php } ?>
-        </tbody>
-    </table>
+            </thead>
+            <tbody>
+                <?php $no = 1;
+                while ($data = mysqli_fetch_assoc($ambildata)) {
+                    if ($data['file'] == "") {
+                        $gambar = "no-image.jpg";
+                    } else {
+                        $gambar = $data['file'];
+                    }
+                ?>
+                    <tr>
+                        <th><?= $no++; ?></th>
+                        <th><?php echo $data['no'] ?></th>
+                        <td><?php echo $data['nama'] ?></td>
+                        <td><?= $data['alamat'] ?></td>
+                        <td><?= $data['nohp'] ?></td>
+                        <td><?= $data['gender'] ?></td>
+                        <td><?= $data['email'] ?></td>
+                        <td><?= $data['nama_kelas'] ?></td>
+                        <td><img class="img-thumbnail" width="100" src="assets/<?= $gambar ?>" /></td>
+                        <td>
+                            <button class="btn btn-info" data-bs-toggle="modal" data-bs-target="#exampleModal" data-bs-id="<?= $data['no'] ?>" data-bs-aksi="ubah"> Ubah
+                            </button>
+                            <button class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#exampleModal" data-bs-id="<?= $data['no'] ?>" data-bs-aksi="hapus"> Hapus
+                            </button>
+                        </td>
+                    </tr>
+                <?php } ?>
+            </tbody>
+        </table>
+    </div>
 
     <!-- Modal -->
     <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
@@ -112,6 +121,11 @@ if ($_SESSION['nama_login'] == null) {
 <!-- BOOTSTRAP JS -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 
+<!-- DATA TABLES  -->
+<script src="https://code.jquery.com/jquery-3.3.1.js"></script>
+<script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.10.19/js/dataTables.bootstrap.min.js"></script>
+
 <Script>
     $(document).ready(function() {
         // alert('Hallo');
@@ -133,6 +147,17 @@ if ($_SESSION['nama_login'] == null) {
             }).always(function() {
                 // alert("finished");
             });
+        });
+    });
+</script>
+
+<!-- PAGINATION DATA TABLES  -->
+<script type="text/javascript">
+    $(document).ready(function() {
+        $('#mytable').DataTable({
+            "language": {
+            "url": "//cdn.datatables.net/plug-ins/1.10.19/i18n/Indonesian.json"
+        }
         });
     });
 </script>
