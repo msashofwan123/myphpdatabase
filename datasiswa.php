3348f401d522e885452354d923125aa294c3e098
msashofwan123 <msashofwan123@gmail.com>
2023-01-20 16:31:09 +0700

    fix link data

--- datasiswa.php
+++ datasiswa.php
@@ -31,7 +31,9 @@ if ($_SESSION['nama_login'] == null) {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
 
     <!-- DATA TABLE -->
-    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.13.1/css/jquery.dataTables.css">
+    <!-- <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.13.1/css/jquery.dataTables.css"> -->
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.13.1/css/dataTables.bootstrap5.min.css">
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.2.0/css/bootstrap.min.css">
 
     <!-- INTERNAL CSS -->
     <link rel="stylesheet" href="index.css">
@@ -128,7 +130,8 @@ if ($_SESSION['nama_login'] == null) {
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 
 <!-- DATA TABLES  -->
-<script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.13.1/js/jquery.dataTables.js"></script>
+<script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.13.1/js/jquery.dataTables.min.js"></script>
+<script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.13.1/js/dataTables.bootstrap5.min.js"></script>
 
 <!-- CUSTOM SCRIPT -->
 <Script>
--- editdata.php
+++ editdata.php
@@ -1,7 +1,6 @@
 <?php
 include_once('database/koneksi.php');
 
-// echo var_dump($_POST);
 // Kolom data di table
 $no = $_POST['no'];
 $nama = $_POST['nama'];
@@ -11,22 +10,116 @@ $gender = $_POST['gender'];
 $email = $_POST['email'];
 $file = $_FILES['file']['name'];
 $id_kelas = $_POST['pilih_kelas'];
+$image = $_FILES['file']['name'];
+$kelas = $_POST['pilih_kelas'];
 
 
 $query = "UPDATE `datasiswa` SET `nama`='$nama',`alamat`='$alamat',`nohp`='$nohp',`gender`='$gender',`email`='$email' ";
-if ($file !='') {
-$query .= ",`file`='$file'";
+if ($file != '') {
+    $query .= ",`file`='$file'";
 }
-$query .="WHERE `no` = '$no'  ";
+$query .= "WHERE `no` = '$no'  ";
 
 
 $update_data = mysqli_query($conn, $query);
 
 if ($update_data) {
-    echo "<h1>Data berhasil masuk</h1><br>
-    <a href='datasiswa.php'>Kembali Ke Tabel</a>
-    ";
-}else{
+?>
+    <html>
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Detail Pendaftaran</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+        <link rel="stylesheet" href="index.css">
+        <link rel="stylesheet" href="postdata.css">
+        <link href="favicon.ico" rel="icon" type="image/x-icon" />
+    </head>
+
+    <body>
+        <br>
+        <br>
+        <br>
+        <br>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <!-- ALERT NOTIFICATION -->
+                    <div class="alert alert-success"><strong>
+                            <center>DATA ANDA TELAH BERHASIL DIPERBAHARUI
+                        </strong></div>
+                </div>
+
+                <div class="col-lg-6">
+                    <table class="table table-primary table-striped">
+                        <thead>
+                            <tr>
+                                <th class="table-success">Data</th>
+                                <th class="table-success">Value</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>Nama</td>
+                                <td><?= $nama; ?></td>
+                            </tr>
+                            <tr>
+                                <td>alamat</td>
+                                <td><?= $alamat; ?></td>
+                            </tr>
+                            <tr>
+                                <td>nohp</td>
+                                <td><?= $nohp; ?></td>
+                            </tr>
+                            <tr>
+                                <td>gender</td>
+                                <td><?= $gender; ?></td>
+                            </tr>
+                            <tr>
+                                <td>email</td>
+                                <td><?= $email; ?></td>
+                            </tr>
+                            <tr>
+                                <td>Kelas</td>
+                                <td><?= $kelas; ?></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+
+                <div class="col-lg-6">
+                    <?php echo '<img src="assets/' . $image . '" class="img-thumbnail" alt=..>'; ?>
+                </div>
+                <div class="col-lg-12 d-grid gap-2">
+                    <a class="btn btn-info" href="datasiswa.php"><strong>MENUJU KE TABEL PENDAFTARAN<div id="timer"></div></strong></a>
+                </div>
+            </div>
+        </div>
+    </body>
+    
+<script>
+    var count=6;
+    function startTimer(){
+        count--;
+        document.getElementById("timer").innerHTML=count;
+        if (count > 0) {
+            setTimeout(startTimer, 1000);
+        }
+    }
+    startTimer();
+</script>
+
+    <script type="text/javascript">
+        setTimeout(function() {
+            window.location = "datasiswa.php";
+        }, 5000);
+    </script>
+
+    </html>
+
+<?php
+} else {
     echo "<h1>Data gagal masuk</h1>";
 }
 
@@ -49,5 +142,4 @@ echo "<br>";
 
 // $nama = $_FILES["foto"]["name"];
 
-move_uploaded_file($_FILES["file"]["tmp_name"], 'assets/'.$file);
-
+move_uploaded_file($_FILES["file"]["tmp_name"], 'assets/' . $file);
